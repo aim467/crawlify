cf9f83f0c25a30ecfff74586132d3a14a36a71bf
aim467 <709544711@qq.com>
2025-05-14 23:20:57 +0800

    feat: 添加 SpiderTask 查询的起止时间筛选功能

--- crawlify-common/src/main/java/org/crawlify/common/dto/query/SpiderTaskQuery.java
+++ crawlify-common/src/main/java/org/crawlify/common/dto/query/SpiderTaskQuery.java
@@ -1,9 +1,21 @@
 package org.crawlify.common.dto.query;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.Data;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import java.time.LocalDateTime;
 
 @Data
 public class SpiderTaskQuery extends Query {
     private String websiteName;
     private Integer status;
+
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+    @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+    private LocalDateTime startTime;
+
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+    @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+    private LocalDateTime endTime;
 }
